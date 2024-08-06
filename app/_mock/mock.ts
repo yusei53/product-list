4a2d1398177a5e6de71fb40bcdce3a5d85e90067
yusei53 <yusei530922@gmail.com>
2024-08-06 23:46:34 +0900

    feat: mock.ts

---
+++ app/_mock/mock.ts
@@ -0,0 +1,22 @@
+import { Product } from "../_types/types";
+
+export const products: Product[] = [
+  {
+    id: "1",
+    name: "Product 1",
+    description: "Description 1",
+    images: "image1.jpg",
+  },
+  {
+    id: "2",
+    name: "Product 2",
+    description: "Description 2",
+    images: "image2.jpg",
+  },
+  {
+    id: "3",
+    name: "Product 3",
+    description: "Description 3",
+    images: "image3.jpg",
+  },
+];
