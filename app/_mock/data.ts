894f669e3efad1822d191ac78ef0935915796010
yusei53 <yusei530922@gmail.com>
2024-08-11 00:57:34 +0900

    add: mock-data

--- app/_mock/data.ts
+++ app/_mock/data.ts
@@ -5,18 +5,30 @@ export const products: Product[] = [
     id: "1",
     name: "Product 1",
     description: "Description 1",
-    image: "test.jpg",
+    image: "/test.jpg",
   },
   {
     id: "2",
     name: "Product 2",
     description: "Description 2",
-    image: "test.jpg",
+    image: "/test.jpg",
   },
   {
     id: "3",
     name: "Product 3",
     description: "Description 3",
-    image: "test.jpg",
+    image: "/test.jpg",
+  },
+  {
+    id: "4",
+    name: "Product 4",
+    description: "Description 4",
+    image: "/test.jpg",
+  },
+  {
+    id: "5",
+    name: "Product 5",
+    description: "Description 5",
+    image: "/test.jpg",
   },
 ];
