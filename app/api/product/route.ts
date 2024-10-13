306bc0538dac7a5fac3c8674730ed7c503f5f7e8
yusei53 <yusei530922@gmail.com>
2024-10-13 23:57:48 +0900

    update: GET API

--- app/api/product/route.ts
+++ app/api/product/route.ts
@@ -3,12 +3,23 @@ import { prisma } from "app/_lib/prisma";
 
 export async function POST(req: NextRequest) {
   try {
-    const { name, description, developer, image } = await req.json();
+    const {
+      title,
+      subtitle,
+      description,
+      skills,
+      department,
+      developer,
+      image,
+    } = await req.json();
 
     const product = await prisma.product.create({
       data: {
-        name,
+        title,
+        subtitle,
         description,
+        skills,
+        department,
         developer,
         image,
       },
