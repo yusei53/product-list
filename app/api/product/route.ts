51ab0ebbbd7b983c34bf8f7f3468c9b3cda4ab69
yusei53 <yusei530922@gmail.com>
2024-10-13 17:50:39 +0900

    fix: 細かい修正

--- app/api/product/route.ts
+++ app/api/product/route.ts
@@ -1,19 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 import { prisma } from "app/_lib/prisma";
 
-export async function GET(req: NextRequest) {
-  try {
-    const products = await prisma.product.findMany();
-    return NextResponse.json(products);
-  } catch (error) {
-    console.error(error);
-    return NextResponse.json(
-      { message: "取得中にエラーが発生しました" },
-      { status: 500 }
-    );
-  }
-}
-
 export async function POST(req: NextRequest) {
   try {
     const { name, description, developer, image } = await req.json();
