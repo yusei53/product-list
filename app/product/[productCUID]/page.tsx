72813aca2764d073c700917dbde206f9394b0fed
yusei53 <yusei530922@gmail.com>
2024-10-17 19:33:30 +0900

    refactor: 詳細ページのリファクタリング

--- app/product/[productCUID]/page.tsx
+++ app/product/[productCUID]/page.tsx
@@ -1,65 +1,9 @@
-"use client";
-import { useDetailStyles } from "app/_theme";
-import Image from "next/image";
-import { createUseStyles } from "react-jss";
+import ProductDetailPage from "./page.client";
 
-const useStyles = createUseStyles({
-  container: {
-    marginTop: "64px",
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  image: {
-    borderRadius: "10px",
-    "@media (max-width: 600px)": {
-      width: "320px",
-      height: "180px",
-    },
-  },
-  content: {
-    maxWidth: "580px",
-    "@media (max-width: 600px)": {
-      maxWidth: "320px",
-    },
-    wordWrap: "break-word",
-  },
-  subtitle: {
-    marginTop: "2px",
-    marginBottom: "4px",
-  },
-});
+// TODO: ここにmetadataを追加する
 
-const Page = () => {
-  useDetailStyles();
-  const classes = useStyles();
-  return (
-    <section className={classes.container}>
-      <article>
-        <div>
-          <Image
-            src="/test.jpg"
-            width={580}
-            height={300}
-            alt=""
-            className={classes.image}
-          />
-          <div className={classes.content}>
-            <h3>product</h3>
-            <h4>概要</h4>
-            <p>
-              NaviTalkは、道案内をしながら目的地までの到達時間を計算する便利なアプリです。あいうえお、道案内をしながら目的地までの到達時間を計算する便利なアプリです。
-            </p>
-            <h4>概要</h4>
-            <p>
-              vnwsvkjnwnbvk;wgbnvd;rwgkp;nvdsrgprrnvsdw;vpbgw;jvnwsripugbnvs;jwbnv;swrpbnvj;wbms;kbnwripbnhsdkw;bkswr
-            </p>
-          </div>
-        </div>
-      </article>
-    </section>
-  );
+const page = () => {
+  return <ProductDetailPage />;
 };
 
-export default Page;
+export default page;
