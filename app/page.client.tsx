6d4f2077801e6a481d74180d4287fb5fc3a8d56a
yusei53 <yusei530922@gmail.com>
2024-10-17 13:32:14 +0900

    update: metadataのための準備

--- app/page.client.tsx
+++ app/page.client.tsx
@@ -2,17 +2,13 @@
 import { useClient } from "app/_utils";
 import { useRouteStyles } from "./_theme";
 import ProductCardList from "./_features/product/list/components/ProductCardList";
-import { FC } from "react";
-import { Product } from "./_features/product/detail/endpoint";
+import { useProductList } from "./_features/product/list/hooks";
 
-type TProps = {
-  productList: Product[];
-};
-
-const TopPage: FC<TProps> = ({ productList }) => {
+const TopPage = () => {
   useRouteStyles();
   const isClient = useClient();
-
+  const { data: productList } = useProductList();
+  if (!productList) return null;
   return (
     isClient && (
       <>
@@ -24,17 +20,3 @@ const TopPage: FC<TProps> = ({ productList }) => {
 };
 
 export default TopPage;
-
-// export const dynamic = "force-dynamic";
-
-// import getProductList from "./_actions/get-product-list";
-// import TopPage from "./page.client";
-
-// const page = async () => {
-//   const productList = await getProductList();
-//   if (!productList) return undefined;
-
-//   return <TopPage productList={productList} />;
-// };
-
-// export default page;
--- app/page.tsx
+++ app/page.tsx
@@ -1,41 +1,9 @@
-"use client";
-import { useClient } from "app/_utils";
-import { useRouteStyles } from "./_theme";
-import ProductCardList from "./_features/product/list/components/ProductCardList";
+import TopPage from "./page.client";
 
-import { useProductList } from "./_features/product/list/hooks";
+// TODO: ここにmetadataを追加する
 
-// type TProps = {
-//   productList: Product[];
-// };
-
-const TopPage = () => {
-  useRouteStyles();
-  const isClient = useClient();
-  const { data: productList } = useProductList();
-  if (!productList) return null;
-  return (
-    isClient && (
-      <>
-        {/* <TopSlider products={products} /> */}
-        <ProductCardList productList={productList} />
-      </>
-    )
-  );
+const page = () => {
+  return <TopPage />;
 };
 
-export default TopPage;
-
-// export const dynamic = "force-dynamic";
-
-// import getProductList from "./_actions/get-product-list";
-// import TopPage from "./page.client";
-
-// const page = async () => {
-//   const productList = await getProductList();
-//   if (!productList) return undefined;
-
-//   return <TopPage productList={productList} />;
-// };
-
-// export default page;
+export default page;
