26bf96268e49e2a43ef49932a30300481718215e
HikariTakahashi <166400513+HikariTakahashi@users.noreply.github.com>
2024-11-09 14:58:53 +0900

    feat:ProductDetaPageにmetadataを設定

--- app/product/[productCUID]/page.tsx
+++ app/product/[productCUID]/page.tsx
@@ -1,6 +1,30 @@
+import { Metadata } from "next";
 import ProductDetailPage from "./page.client";
 
-// TODO: ここにmetadataを追加する
+const siteName = "ぴーちフォリオ | PeachTechポートフォリオ";
+const description =
+  "PeachTechのメンバーが作成したポートフォリオサイトです。個人の趣味で作ったものから大会に出したようなプロダクトまで一人一人の個性、想いがつまったプロダクトをお楽しみください！";
+
+export const metadata: Metadata = {
+  metadataBase: new URL("https://peachtech-product-list.vercel.app/product/"),
+  title: {
+    default: siteName,
+    template: "%s | Product list",
+  },
+  description: description,
+  openGraph: {
+    type: "website",
+    url: "https://peachtech-product-list.vercel.app/product/",
+    title: siteName,
+    description: description,
+    siteName: siteName,
+  },
+  twitter: {
+    title: siteName,
+    description: description,
+    card: "summary",
+  },
+};
 
 const page = () => {
   return <ProductDetailPage />;
